ae7bd9e83bf809dfd231a5094ce8665ebce03712
Jacques <jacques.laffy@gmail.com>
2019-02-07 16:27:57 +0000

    Added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require bootstrap
-//= require rails-ujs
-//= require_tree .
+//= require bootstrap-sprockets
+//= require jquery
